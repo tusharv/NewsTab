815b2abf60a24f71d007cd541810fb9b4ec9a376
Tushar Vaghela <tushar.v@media.net>
2020-01-27 10:36:44 +0530

    NewsTab First Release

---
+++ README.md
@@ -0,0 +1,38 @@
+# NewsTab
+
+News at your NewTab.
+
+This extension is for those who needs news from all sources.
+
+You can select country, category or search news from serchbox.
+
+News around the world.
+News as it happens.
+News at your NewTab.
+Unbiased news, news from different source and perspective.
+
+Powered by https://newsapi.org/
+
+Icon made by https://freeicons.io/profile/730 from www.freeicons.io
+
+
+## Development
+
+1. Clone this repo
+```git clone https://github.com/tusharv/NewsTab```
+
+2. Make your changes.
+3. To test, go to: chrome://extensions, enable Developer mode and load src as an unpacked extension.
+
+## Credits
+- News from https://newsapi.org/
+- Icon made by https://freeicons.io/profile/730 from www.freeicons.io
+
+## Issues
+
+Feel free to submit issues and feature requests.
+
+## License
+
+Copyright (c) 2020 Tushar Vaghela
+Licensed under the [GNU General Public License v3.0](https://github.com/tusharv/NewsTab/blob/master/LICENSE).
---
+++ src/js/script.js
@@ -0,0 +1,496 @@
+const DETAULTS = {
+    COUNTRY: "in",
+    CATEGORY: "general",
+    PAGESIZE: 25
+  }
+  
+  const URLS = {
+    FEEDURL: "https://newsapi.org/v2/top-headlines?country=#COUNTRY#&category=#CATEGORY#&pagesize=#PAGESIZE#",
+    QUERYURL: "https://newsapi.org/v2/everything?q=#QUERY#&from=#DATE#&sortBy=publishedAt",
+    FALLBACK_IMAGE: 'https://via.placeholder.com/500/000000/FFFFFF/?text=Image%20Not%20Found',
+  }
+  
+  const LOCALSTORAGE = {
+    COUNTRY: 'country',
+    CATEGORY: 'category',
+    PAGESIZE: 'pagesize'
+  }
+  
+  const COUNTRY_LIST = {
+    ae:{
+      name:'ae',
+      display:'United Arab Emirates',
+      flag:'ae.png'
+      },
+    ar:{
+        name:'ar',
+        display:'Argentina',
+        flag:'ar.png'
+        },
+    at:{
+        name:'at',
+        display:'Austria',
+        flag:'at.png'
+        },
+    au:{
+        name:'au',
+        display:'Australia',
+        flag:'au.png'
+        },
+    be:{
+        name:'be',
+        display:'Belgium',
+        flag:'be.png'
+        },
+    bg:{
+        name:'bg',
+        display:'Bulgaria',
+        flag:'bg.png'
+        },
+    br:{
+        name:'br',
+        display:'Brazil',
+        flag:'br.png'
+        },
+    ca:{
+        name:'ca',
+        display:'Canada',
+        flag:'ca.png'
+        },
+    ch:{
+        name:'ch',
+        display:'Switzerland',
+        flag:'ch.png'
+        },
+    cn:{
+        name:'cn',
+        display:'China',
+        flag:'cn.png'
+    },
+    co:{
+        name:'co',
+        display:'Colombia',
+        flag:'co.png'
+    },
+    cu:{
+        name:'cu',
+        display:'Cuba',
+        flag:'cu.png'
+    },
+    cz:{
+        name:'cz',
+        display:'Czechia',
+        flag:'cz.png'
+    },
+    de:{
+        name:'de',
+        display:'Germany',
+        flag:'de.png'
+    },
+    eg:{
+        name:'eg',
+        display:'Egypt',
+        flag:'eg.png'
+    },
+    fr:{
+        name:'fr',
+        display:'France',
+        flag:'fr.png'
+    },
+    gb:{
+        name:'gb',
+        display:'United Kingdom',
+        flag:'gb.png'
+    },
+    gr:{
+        name:'gr',
+        display:'Greece',
+        flag:'gr.png'
+    },
+    hk:{
+        name:'hk',
+        display:'Hong Kong',
+        flag:'hk.png'
+    },
+    hu:{
+        name:'hu',
+        display:'Hungary',
+        flag:'hu.png'
+    },
+    id:{
+        name:'id',
+        display:'Indonesia',
+        flag:'id.png'
+    },
+    ie:{
+        name:'ie',
+        display:'Ireland',
+        flag:'ie.png'
+    },
+    il:{
+        name:'il',
+        display:'Israel',
+        flag:'il.png'
+    },
+    in:{
+        name:'in',
+        display:'India',
+        flag:'in.png'
+    },
+    it:{
+        name:'it',
+        display:'Italy',
+        flag:'it.png'
+    },
+    jp:{
+        name:'jp',
+        display:'Japan',
+        flag:'jp.png'
+    },
+    kr:{
+        name:'kr',
+        display:'Republic of Korea',
+        flag:'kr.png'
+    },
+    lt:{
+        name:'lt',
+        display:'Lithuania',
+        flag:'lt.png'
+    },
+    lv:{
+        name:'lv',
+        display:'Latvia',
+        flag:'lv.png'
+    },
+    ma:{
+        name:'ma',
+        display:'Morocco',
+        flag:'ma.png'
+    },
+    mx:{
+        name:'mx',
+        display:'Mexico',
+        flag:'mx.png'
+    },
+    my:{
+        name:'my',
+        display:'Malaysia',
+        flag:'my.png'
+    },
+    ng:{
+        name:'ng',
+        display:'Nigeria',
+        flag:'ng.png'
+    },
+    nl:{
+        name:'nl',
+        display:'Netherlands',
+        flag:'nl.png'
+    },
+    no:{
+        name:'no',
+        display:'Norway',
+        flag:'no.png'
+    },
+    nz:{
+        name:'nz',
+        display:'New Zealand',
+        flag:'nz.png'
+    },
+    ph:{
+        name:'ph',
+        display:'Philippines',
+        flag:'ph.png'
+    },
+    pl:{
+        name:'pl',
+        display:'Poland',
+        flag:'pl.png'
+    },
+    pt:{
+        name:'pt',
+        display:'Portugal',
+        flag:'pt.png'
+    },
+    ro:{
+        name:'ro',
+        display:'Romania',
+        flag:'ro.png'
+    },
+    rs:{
+        name:'rs',
+        display:'Serbia',
+        flag:'rs.png'
+    },
+    ru:{
+        name:'ru',
+        display:'Russian Federation',
+        flag:'ru.png'
+    },
+    sa:{
+        name:'sa',
+        display:'Saudi Arabia',
+        flag:'sa.png'
+    },
+    se:{
+        name:'se',
+        display:'Sweden',
+        flag:'se.png'
+    },
+    sg:{
+        name:'sg',
+        display:'Singapore',
+        flag:'sg.png'
+    },
+    si:{
+        name:'si',
+        display:'Slovenia',
+        flag:'si.png'
+    },
+    sk:{
+        name:'sk',
+        display:'Slovakia',
+        flag:'sk.png'
+    },
+    th:{
+        name:'th',
+        display:'Thailand',
+        flag:'th.png'
+    },
+    tr:{
+        name:'tr',
+        display:'Turkey',
+        flag:'tr.png'
+    },
+    tw:{
+        name:'tw',
+        display:'Taiwan',
+        flag:'tw.png'
+    },
+    ua:{
+        name:'ua',
+        display:'Ukraine',
+        flag:'ua.png'
+    },
+    us:{
+        name:'us',
+        display:'United States of America',
+        flag:'us.png'
+    },
+    ve:{
+        name:'ve',
+        display:'Venezuela',
+        flag:'ve.png'
+    },
+    za:{
+        name:'za',
+        display:'South Africa',
+        flag:'za.png'
+    }
+  }
+  
+  const CATEGORY_LIST = {
+    business: {
+      name: 'business',
+      display: 'Business'
+    },
+    entertainment: {
+      name: 'entertainment',
+      display: 'Entertainment'
+    },
+    general: {
+      name: 'general',
+      display: 'General'
+    },
+    health: {
+      name: 'health',
+      display: 'Health'
+    },
+    science: {
+      name: 'science',
+      display: 'Science'
+    },
+    sports: {
+      name: 'sports',
+      display: 'Sports'
+    },
+    technology: {
+      name: 'technology',
+      display: 'Technology'
+    },
+  }
+  
+  function fetchFeed(url) {
+    makeRequest(url)
+      .then(res => {
+        let { articles, totalResults } = JSON.parse(res.responseText);
+        loadNews(articles, totalResults);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  
+  function loadNews(articles, totalResults) {
+    let newsContainer = document.querySelector(".news");
+    let template = document.querySelector("#newstemplate");
+    let results = document.querySelector('.newsResults');
+    results.innerHTML = `Total <strong>${totalResults}</strong> results found`;
+    
+  
+    articles.forEach((article, index) => {
+
+      if(article.author === null){
+        article.author = article.source.name;
+      }
+  
+      if(article.content === null){
+        article.content = "Click link to know more..";
+      }
+      
+      if (article.author && article.content) {
+        let newsNode = document.importNode(template.content, true);
+  
+        let title = newsNode.querySelector(".newsTitle");
+        title.innerText = article.title;
+        title.setAttribute("href",article.url);
+  
+        let count = newsNode.querySelector('.newsCount')
+        count.innerText = `#${index +1}`;
+  
+        let author = newsNode.querySelector(".newsAuthor");
+        author.innerHTML =
+          `<strong>${article.author}</strong> Published about ${moment(article.publishedAt).startOf('m').fromNow()}`;
+  
+        let desc = newsNode.querySelector(".newsDescription");
+        desc.innerHTML = article.content;
+  
+        let img = newsNode.querySelector(".newsImage");
+        img.src = article.urlToImage || URLS.FALLBACK_IMAGE;
+        newsContainer.appendChild(newsNode);
+        
+        img.addEventListener("error", function(e){
+          e.target.setAttribute("src", URLS.FALLBACK_IMAGE);
+        });
+        
+      }
+    });
+  }
+  
+  
+  function makeRequest(url, method) {
+    var request = new XMLHttpRequest();
+    return new Promise(function(resolve, reject) {
+      request.onreadystatechange = function() {
+        if (request.readyState !== 4) return;
+  
+        if (request.status >= 200 && request.status < 300) {
+          resolve(request);
+        } else {
+          reject({
+            status: request.status,
+            statusText: request.statusText
+          });
+        }
+      }
+      request.open(method || "GET", url, true);
+      //News API is Free - Please use your own Key from https://newsapi.org/ :)
+      request.setRequestHeader('X-Api-Key', '4c2159b38736423cb393853bda9e642f');
+      request.send();
+    });
+  }
+  
+  function onSearch(e){
+    if (e.keyCode === 13 && e.target.value.length > 0) {
+      window.location.href = `${window.location.protocol}//${window.location.host}${window.location.pathname}?q=${e.target.value}`;
+    }
+  }
+  
+  function getParams(url) {
+      let params = {};
+      let parser = document.createElement('a');
+      parser.href = url;
+      let query = parser.search.substring(1);
+      let vars = query.split('&');
+      for (let i = 0; i < vars.length; i++) {
+          let pair = vars[i].split('=');
+          params[pair[0]] = decodeURIComponent(pair[1]);
+      }
+      return params;
+  }
+  
+  function updateCountry(e){
+    localStorage.setItem(LOCALSTORAGE.COUNTRY, e.target.value);
+    window.location.href = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+  }
+  
+  function updateCategory(e){
+    localStorage.setItem(LOCALSTORAGE.CATEGORY, e.target.value);
+    window.location.href = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+  }
+  
+  function populateList(elem, group, key){
+    let list = Object.keys(group);
+    for(var i=0;i<list.length;i++){ 
+      let option = document.createElement('option');
+      let selected = localStorage.getItem(key);
+      option.value = group[list[i]].name;
+      option.innerText = group[list[i]].display;
+  
+      if(selected === list[i]){
+        option.selected = true;
+      }
+  
+      elem.appendChild(option);
+    }
+  }
+  
+  function updateTitle(){
+    let country = COUNTRY_LIST[(localStorage.getItem(LOCALSTORAGE.COUNTRY) || DETAULTS.COUNTRY)].display;
+    let category = CATEGORY_LIST[(localStorage.getItem(LOCALSTORAGE.CATEGORY) || DETAULTS.CATEGORY)].display;
+  
+    document.querySelector('title').innerText = `${category} News from ${country}`;
+  }
+  
+  function init() {
+    let search = document.getElementById('search');
+    let d = new Date();
+    
+    if(search) search.addEventListener('keydown', onSearch);
+    
+    let param = getParams(window.location.href);
+    
+    if(param.q){
+      search.value = param.q;
+      fetchFeed(
+        URLS.QUERYURL
+          .replace('#QUERY#',param.q)
+          .replace('#DATE#',`${d.getFullYear()}-${"0" + d.getMonth()+1}-${d.getDate()}`
+      ));
+    }else{
+      fetchFeed(
+        URLS.FEEDURL
+          .replace('#COUNTRY#', (localStorage.getItem(LOCALSTORAGE.COUNTRY) || DETAULTS.COUNTRY))
+          .replace('#CATEGORY#', (localStorage.getItem(LOCALSTORAGE.CATEGORY) || DETAULTS.CATEGORY))
+          .replace('#PAGESIZE#', (localStorage.getItem(LOCALSTORAGE.PAGESIZE) || DETAULTS.PAGESIZE))
+        );  
+    }
+  
+    
+    let countrySelector = document.querySelector('#countrySelector');
+    let categorySelector = document.querySelector('#categorySelector');
+    
+    populateList(countrySelector, COUNTRY_LIST, LOCALSTORAGE.COUNTRY);
+    populateList(categorySelector, CATEGORY_LIST, LOCALSTORAGE.CATEGORY);
+    
+    countrySelector.addEventListener('change', updateCountry);
+    categorySelector.addEventListener('change', updateCategory);
+  
+    updateTitle();
+  }
+  
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+  } else {
+    init();
+  }
